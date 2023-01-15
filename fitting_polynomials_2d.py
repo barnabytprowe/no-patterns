d56a74383113ef704ea20040a9b9188e0efdfbda
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-01-15 17:47:09 +0000

    refactoring, save output into timestamps subfolders

--- fitting_polynomials_2d.py
+++ fitting_polynomials_2d.py
@@ -1,14 +1,15 @@
 """
-pattern_example_2d.py
-=====================
+fitting_polynomials_2d.py
+=========================
 """
 import os
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn.preprocessing import PolynomialFeatures, SplineTransformer
+
+import sklearn.linear_model
+import sklearn.preprocessing
 
 
 # Parameters
@@ -20,84 +21,94 @@ nx = 32
 # Sigma of iid pixel noise
 noise_sigma = 1.
 
-# True, low (insufficient) order and high (overfitting) order
-fit_degree0 = 8
+# True, low (insufficient) and high (overfitting) polynomial order to use when fitting
+fit_degree_true = 8  # the real signal in the simulations will be a 2D polnoymial of this order
 fit_degree_lo = 2
 fit_degree_hi = 32
 
-# Per coefficient "signal to noise" in random true pattern
+# Per coefficient "signal to noise" in random true pattern, i.e. ratio of standard deviation
+# of true curve coefficient values to noise_sigma
 coeff_signal_to_noise = 8.
+
+
+# Functions
+# =========
+
+def polynomial_design_matrix(X, degree):
+	"""Returns polynomial design matrix for input coordinates and polynomial degree.
+
+	Args:
+		X:
+			array-like of shape (n_samples, n_dimensions), the data to input to
+			the polynomial features
+		degree: maximum polynomial degree of features in all dimensions
+	"""
+	poly_features = sklearn.preprocessing.PolynomialFeatures(degree=degree)
+	return poly_features.fit_transform(X, y=None)
 
 
 # Main script
 # ===========
 
+# Current timestamp, used in I/0
 tstmp =  pd.Timestamp.now().isoformat().replace(":", "")
+outdir = os.path.join(".", "plots", tstmp)
+os.mkdir(outdir)
 
-# Define unit interval and x, y grid coords for centroids of a spanning square grid
+# Define x, y grid coords for centroids of a unit square spanning square grid
+# centred on the origin
 xvals = np.linspace(-.5 + 1./(2. * nx), .5 - 1./(2. * nx), num=nx, endpoint=True)
 Xxgrid, Xygrid = np.meshgrid(xvals, xvals)
 
-# Define design matrix and transform to polynomial linear feature (?)
+# Use sklearn PolynomialFeatures to model simple polynomials in 2D
+# x, y coordinates
 X = np.stack([Xxgrid.flatten(), Xygrid.flatten()], axis=1)
-if True:
-	poly_true = PolynomialFeatures(degree=fit_degree0)
-	poly_fit_lo = PolynomialFeatures(degree=fit_degree_lo)
-	poly_fit_hi = PolynomialFeatures(degree=fit_degree_hi)
-	ftype = "poly"
-else:
-	poly_true = SplineTransformer(degree=3, n_knots=2)#, extrapolation="periodic")
-	poly_fit_lo = SplineTransformer(degree=3, n_knots=fit_degree0)#, extrapolation="periodic")
-	poly_fit_hi = SplineTransformer(degree=3, n_knots=fit_degree_hi)#, extrapolation="periodic")
-	ftype = "b-spline"
+# Design matrices for the true, too low and too high cases
+features_true = polynomial_design_matrix(X=X, degree=fit_degree_true)
+features_fit_lo = polynomial_design_matrix(X=X, degree=fit_degree_lo)
+features_fit_hi = polynomial_design_matrix(X=X, degree=fit_degree_hi)
 
-features_true = poly_true.fit_transform(X)
-features_fit_lo = poly_fit_lo.fit_transform(X)
-features_fit_hi = poly_fit_hi.fit_transform(X)
-
-# Build the true curve
+# Build the true 2D contour and plot
 ctrue = np.random.randn(features_true.shape[-1]) * coeff_signal_to_noise
 ztrue = (np.matmul(features_true, ctrue)).reshape((nx, nx))
 plt.title("Ideal model curve")
 plt.pcolor(ztrue, cmap="Greys"); plt.colorbar()
-plt.savefig(os.path.join(".", "plots", "ideal_"+tstmp+".png"))
+plt.savefig(os.path.join(outdir, "ideal_"+tstmp+".png"))
 plt.show()
 
-# Add the random noise to generate the dataset
+# Add the random noise to generate the dataset, and plot
 zdata = ztrue + noise_sigma * np.random.randn(*ztrue.shape)
 plt.title("Data")
 plt.pcolor(zdata, cmap="Greys"); plt.colorbar()
-plt.savefig(os.path.join(".", "plots", "data_"+tstmp+".png"))
+plt.savefig(os.path.join(outdir, "data_"+tstmp+".png"))
 plt.show()
 
-# Perform low order, true and high order regressions
-regr_lo = linear_model.LinearRegression()
-regr_lo.fit(features_fit_lo, zdata.flatten())
-pred_lo = regr_lo.predict(features_fit_lo).reshape((nx, nx))
+# Perform too low order, true and too high order regressions
+predictions = []
+zflat = zdata.flatten()
+for features in (features_fit_lo, features_true, features_fit_hi):
 
-regr0 = linear_model.LinearRegression()
-regr0.fit(features_true, zdata.flatten())
-pred0 = regr0.predict(features_true).reshape((nx, nx))
+	regr = sklearn.linear_model.LinearRegression()
+	regr.fit(features, zflat)
+	predictions.append(regr.predict(features).reshape((nx, nx)))
 
-regr_hi = linear_model.LinearRegression()
-regr_hi.fit(features_fit_hi, zdata.flatten())
-pred_hi = regr_hi.predict(features_fit_hi).reshape((nx, nx))
+pred_lo, pred_true, pred_hi = tuple(predictions)
 
 # Plot residuals
 plt.pcolor(zdata - pred_lo, cmap="Greys"); plt.colorbar(); plt.clim([-2.5, 2.5])
 plt.title("Low order polynomial fit residuals")
-plt.savefig(os.path.join(".", "plots", "lo_"+tstmp+".png"))
+plt.savefig(os.path.join(outdir, "lo_"+tstmp+".png"))
 plt.show()
 rlo = zdata - pred_lo
 
-plt.pcolor(zdata - pred0, cmap="Greys"); plt.colorbar(); plt.clim([-2.5, 2.5])
+plt.pcolor(zdata - pred_true, cmap="Greys"); plt.colorbar(); plt.clim([-2.5, 2.5])
 plt.title("Matching order polynomial fit residuals")
-plt.savefig(os.path.join(".", "plots", "matching_"+tstmp+".png"))
+plt.savefig(os.path.join(outdir, "matching_"+tstmp+".png"))
 plt.show()
-r0 = zdata - pred0
+rtrue = zdata - pred_true
 
 plt.pcolor(zdata - pred_hi, cmap="Greys"); plt.colorbar(); plt.clim([-2.5, 2.5])
 plt.title("High order polynomial fit residuals")
-plt.savefig(os.path.join(".", "plots", "hi_"+tstmp+".png"))
+plt.savefig(os.path.join(outdir, "hi_"+tstmp+".png"))
 plt.show()
 rhi = zdata - pred_hi
