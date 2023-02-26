d844af8c815bd43d36ddc2c10117188d72b09565
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-02-26 14:39:32 +0000

    slight refactor

--- symmetric_toeplitz_entropy.py
+++ symmetric_toeplitz_entropy.py
@@ -15,19 +15,13 @@ def exponential_decay(ndim, decay_factor=0., circular=False):
     a constant decay_factor with each increasing index i, where array[0] = 1,
     with optional circular symmetry.
     """
+    _rarr = np.zeros(ndim, dtype=float)
     if not circular:
-        
+        _rarr[0] = 1.
         if decay_factor > 0.:
-            return (1. / decay_factor) * np.full(ndim, decay_factor, dtype=float).cumprod()
-
-        else:
-            _rarr = np.zeros(ndim, dtype=float)
-            _rarr[0] = 1.
-            return _rarr
-
+            _rarr[1:] = np.full(ndim - 1, decay_factor, dtype=float).cumprod()
     else:
-        _rarr = np.zeros(ndim, dtype=float)
-        _first_half = lagged_correlation(1 + ndim//2, decay_factor=decay_factor, circular=False)
+        _first_half = exponential_decay(1 + ndim//2, decay_factor=decay_factor, circular=False)
         _rarr[:(1 + ndim//2)] = _first_half
-        _rarr[(1 + ndim//2):] = _first_half[1:(ndim + 1)//2][::-1]
-        return _rarr
+        _rarr[(1 + ndim//2):] = _first_half[1:(1 + ndim)//2][::-1]
+    return _rarr
