4bc0015719ecae08417f6f5b1ef6464495b9bfd8
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-03-03 22:21:52 +0000

    put some barebones stats calc functionality in

--- stats_polynomials_2d.py
+++ stats_polynomials_2d.py
@@ -3,3 +3,70 @@ stats_polynomials_2d.py
 =======================
 
 """
+
+import collections
+import glob
+import os
+import pickle
+import yaml
+
+import numpy as np
+import pandas as pd
+
+
+# Params
+# ------
+
+DEFAULT_ROOTDIR = os.path.join(".", "plots")
+_tstamps = ["2023-03-02T224853.137222", "2023-03-03T132122.559540"]
+
+
+# Functions
+# ---------
+
+# def roll2d(arr, shift0, shift1=None):
+#   """
+#   """
+
+def pathfile(timestamp, rootdir=DEFAULT_ROOTDIR):
+    _tfolder = os.path.join(rootdir, timestamp)
+    if not os.path.isdir(_tfolder):
+        raise FileNotFoundError(_tfolder)
+
+    return _tfolder, os.path.join(_tfolder, f"output_{timestamp}.pickle")
+
+def get_stats(file):
+    with open(file, "rb") as fin:
+        data = pickle.load(fin)
+
+    rstats = {}
+    for _order in ("lo", "true", "hi"):
+
+        _res = data[f"r{_order}"]
+        rstats[_order] = {
+            "file": file,
+            "ctrue": data["ctrue"],
+            "ztrue": data["ztrue"],
+            "zdata": data["zdata"],
+            "residuals": _res,
+            "RSS": np.sum(_res**2),
+            "rho": np.fft.ifft2(np.abs(np.fft.fft2(_res))**2).real / np.product(_res.shape),
+            "cps": np.abs(np.fft.fft2(_res))**2,
+        }
+
+    return tuple(rstats[_o] for _o in ("lo", "true", "hi"))
+
+
+if __name__ == "__main__":
+
+    # Loop through timestamps
+    for _ts in _tstamps:
+
+        _tfolder, _tsfile = pathfile(_ts, rootdir=DEFAULT_ROOTDIR)
+        print(f"Building stats from {_tsfile}")
+        _lo, _true, _hi = get_stats(_tsfile)
+        stats = {"lo": _lo, "true": _true, "hi": _hi}
+        print("RSS (lo, true, hi):")
+        print(tuple(stats[_order]["RSS"] for _order in ("lo", "true", "hi")))
+        statfile = os.path.join(_tfolder, f"stats_{_ts}.pickle")
+        #import ipdb; ipdb.set_trace()
