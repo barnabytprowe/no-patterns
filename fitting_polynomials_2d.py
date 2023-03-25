fcedfc0c160f5a3344b3827fd388947653263ec8
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-03-25 00:14:43 +0000

    indent with spaces, move to if main block

--- fitting_polynomials_2d.py
+++ fitting_polynomials_2d.py
@@ -46,109 +46,111 @@ TITLE_SIZE = "x-large"
 # =========
 
 def polynomial_design_matrix(X, degree):
-	"""Returns polynomial design matrix for input coordinates and polynomial degree.
+    """Returns polynomial design matrix for input coordinates and polynomial degree.
 
-	Args:
-		X:
-			array-like of shape (n_samples, n_dimensions), the data to input to
-			the polynomial features
-		degree: maximum polynomial degree of features in all dimensions
-	"""
-	poly_features = sklearn.preprocessing.PolynomialFeatures(degree=degree)
-	return poly_features.fit_transform(X, y=None)
+    Args:
+        X:
+            array-like of shape (n_samples, n_dimensions), the data to input to
+            the polynomial features
+        degree: maximum polynomial degree of features in all dimensions
+    """
+    poly_features = sklearn.preprocessing.PolynomialFeatures(degree=degree)
+    return poly_features.fit_transform(X, y=None)
 
 
 # Main script
 # ===========
 
-# Current timestamp, used in I/0
-tstmp =  pd.Timestamp.now().isoformat().replace(":", "")
-outdir = os.path.join(".", "plots", tstmp)
-os.mkdir(outdir)
+if __name__ == "__main__":
 
-# Output dict - will be pickled
-output = {}
+    # Current timestamp, used in I/0
+    tstmp =  pd.Timestamp.now().isoformat().replace(":", "")
+    outdir = os.path.join(".", "plots", tstmp)
+    os.mkdir(outdir)
 
-# Define x, y grid coords for centroids of a unit square spanning square grid
-# centred on the origin
-xvals = np.linspace(-.5 + 1./(2. * nx), .5 - 1./(2. * nx), num=nx, endpoint=True)
-Xxgrid, Xygrid = np.meshgrid(xvals, xvals)
+    # Output dict - will be pickled
+    output = {}
 
-# Use sklearn PolynomialFeatures to model simple polynomials in 2D
-# x, y coordinates
-X = np.stack([Xxgrid.flatten(order="C"), Xygrid.flatten(order="C")], axis=1)
-# Design matrices for the true, too low and too high cases
-features_true = polynomial_design_matrix(X=X, degree=fit_degree_true)
-features_fit_lo = polynomial_design_matrix(X=X, degree=fit_degree_lo)
-features_fit_hi = polynomial_design_matrix(X=X, degree=fit_degree_hi)
+    # Define x, y grid coords for centroids of a unit square spanning square grid
+    # centred on the origin
+    xvals = np.linspace(-.5 + 1./(2. * nx), .5 - 1./(2. * nx), num=nx, endpoint=True)
+    Xxgrid, Xygrid = np.meshgrid(xvals, xvals)
 
-# Build the true 2D contour and plot
-ctrue = np.random.randn(features_true.shape[-1]) * coeff_signal_to_noise
-ztrue = (np.matmul(features_true, ctrue)).reshape((nx, nx), order="C")
-output["ctrue"] = ctrue
-output["ztrue"] = ztrue
+    # Use sklearn PolynomialFeatures to model simple polynomials in 2D
+    # x, y coordinates
+    X = np.stack([Xxgrid.flatten(order="C"), Xygrid.flatten(order="C")], axis=1)
+    # Design matrices for the true, too low and too high cases
+    features_true = polynomial_design_matrix(X=X, degree=fit_degree_true)
+    features_fit_lo = polynomial_design_matrix(X=X, degree=fit_degree_lo)
+    features_fit_hi = polynomial_design_matrix(X=X, degree=fit_degree_hi)
 
-fig = plt.figure(figsize=FIGSIZE)
-plt.title("Ideal model curve", size=TITLE_SIZE)
-plt.pcolor(ztrue, cmap=CMAP); plt.colorbar()
-plt.tight_layout()
-plt.savefig(os.path.join(outdir, "ideal_"+tstmp+".png"))
-plt.show()
+    # Build the true 2D contour and plot
+    ctrue = np.random.randn(features_true.shape[-1]) * coeff_signal_to_noise
+    ztrue = (np.matmul(features_true, ctrue)).reshape((nx, nx), order="C")
+    output["ctrue"] = ctrue
+    output["ztrue"] = ztrue
 
-# Add the random noise to generate the dataset, and plot
-zdata = ztrue + noise_sigma * np.random.randn(*ztrue.shape)
-output["zdata"] = zdata
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.title("Ideal model curve", size=TITLE_SIZE)
+    plt.pcolor(ztrue, cmap=CMAP); plt.colorbar()
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir, "ideal_"+tstmp+".png"))
+    plt.show()
 
-fig = plt.figure(figsize=FIGSIZE)
-plt.title("Data", size=TITLE_SIZE)
-plt.pcolor(zdata, cmap=CMAP); plt.colorbar()
-plt.tight_layout()
-plt.savefig(os.path.join(outdir, "data_"+tstmp+".png"))
-plt.show()
+    # Add the random noise to generate the dataset, and plot
+    zdata = ztrue + noise_sigma * np.random.randn(*ztrue.shape)
+    output["zdata"] = zdata
 
-# Perform too low order, true and too high order regressions
-predictions = []
-zflat = zdata.flatten(order="C")
-for features in (features_fit_lo, features_true, features_fit_hi):
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.title("Data", size=TITLE_SIZE)
+    plt.pcolor(zdata, cmap=CMAP); plt.colorbar()
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir, "data_"+tstmp+".png"))
+    plt.show()
 
-	regr = sklearn.linear_model.LinearRegression()
-	regr.fit(features, zflat)
-	predictions.append(regr.predict(features).reshape((nx, nx), order="C"))
+    # Perform too low order, true and too high order regressions
+    predictions = []
+    zflat = zdata.flatten(order="C")
+    for features in (features_fit_lo, features_true, features_fit_hi):
 
-pred_lo, pred_true, pred_hi = tuple(predictions)
-output["pred_lo"] = pred_lo
-output["pred_true"] = pred_true
-output["pred_hi"] = pred_hi
+        regr = sklearn.linear_model.LinearRegression()
+        regr.fit(features, zflat)
+        predictions.append(regr.predict(features).reshape((nx, nx), order="C"))
 
-# Plot residuals
-fig = plt.figure(figsize=FIGSIZE)
-plt.pcolor(zdata - pred_lo, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
-plt.title("Low order polynomial fit residuals", size=TITLE_SIZE)
-plt.tight_layout()
-plt.savefig(os.path.join(outdir, "lo_"+tstmp+".png"))
-plt.show()
-rlo = zdata - pred_lo
-output["rlo"] = rlo
+    pred_lo, pred_true, pred_hi = tuple(predictions)
+    output["pred_lo"] = pred_lo
+    output["pred_true"] = pred_true
+    output["pred_hi"] = pred_hi
 
-fig = plt.figure(figsize=FIGSIZE)
-plt.pcolor(zdata - pred_true, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
-plt.title("Matching order polynomial fit residuals", size=TITLE_SIZE)
-plt.tight_layout()
-plt.savefig(os.path.join(outdir, "matching_"+tstmp+".png"))
-plt.show()
-rtrue = zdata - pred_true
-output["rtrue"] = rtrue
+    # Plot residuals
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.pcolor(zdata - pred_lo, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
+    plt.title("Low order polynomial fit residuals", size=TITLE_SIZE)
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir, "lo_"+tstmp+".png"))
+    plt.show()
+    rlo = zdata - pred_lo
+    output["rlo"] = rlo
 
-fig = plt.figure(figsize=FIGSIZE)
-plt.pcolor(zdata - pred_hi, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
-plt.title("High order polynomial fit residuals", size=TITLE_SIZE)
-plt.tight_layout()
-plt.savefig(os.path.join(outdir, "hi_"+tstmp+".png"))
-plt.show()
-rhi = zdata - pred_hi
-output["rhi"] = rhi
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.pcolor(zdata - pred_true, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
+    plt.title("Matching order polynomial fit residuals", size=TITLE_SIZE)
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir, "matching_"+tstmp+".png"))
+    plt.show()
+    rtrue = zdata - pred_true
+    output["rtrue"] = rtrue
 
-outfile = os.path.join(outdir, "output_"+tstmp+".pickle")
-print("Saving to "+outfile)
-with open(outfile, "wb") as fout:
-    pickle.dump(output, fout)
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.pcolor(zdata - pred_hi, cmap=CMAP); plt.colorbar(); plt.clim([-2.5, 2.5])
+    plt.title("High order polynomial fit residuals", size=TITLE_SIZE)
+    plt.tight_layout()
+    plt.savefig(os.path.join(outdir, "hi_"+tstmp+".png"))
+    plt.show()
+    rhi = zdata - pred_hi
+    output["rhi"] = rhi
+
+    outfile = os.path.join(outdir, "output_"+tstmp+".pickle")
+    print("Saving to "+outfile)
+    with open(outfile, "wb") as fout:
+        pickle.dump(output, fout)
