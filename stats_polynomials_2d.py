8bd7d9b02132680f31bd9648a9c978616362118d
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-03-25 00:15:05 +0000

    add shuffled plotting to the stats package

--- stats_polynomials_2d.py
+++ stats_polynomials_2d.py
@@ -12,6 +12,11 @@ import yaml
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from fitting_polynomials_2d import FIGSIZE
+from fitting_polynomials_2d import CMAP
+from fitting_polynomials_2d import TITLE_SIZE
 
 
 # Params
@@ -25,6 +30,9 @@ TIMESTAMPS = [os.path.basename(_p) for _p in glob.glob(os.path.join(DEFAULT_ROOT
 # ---------
 
 def pathfile(timestamp, rootdir=DEFAULT_ROOTDIR):
+    """Returns the folder and output pickle filename corresponding to a run
+    of fitting_polynomials_2d.py for the given timestamp.
+    """
     _tfolder = os.path.join(rootdir, timestamp)
     if not os.path.isdir(_tfolder):
         raise FileNotFoundError(_tfolder)
@@ -32,16 +40,26 @@ def pathfile(timestamp, rootdir=DEFAULT_ROOTDIR):
     return _tfolder, os.path.join(_tfolder, f"output_{timestamp}.pickle")
 
 
-def get_stats(file):
-    with open(file, "rb") as fin:
+def get_stats(timestamp, rng=None):
+    """Retrieves, calculates and returns key stats from a
+    fitting_polynomials_2d.py output pickle file for a given timestamp, and
+    generates randomized (shuffled) versions of the residual plots.
+    """
+    if rng is None:
+        rng = np.random.default_rng()
+
+    _tfolder, _tsfile = pathfile(timestamp, rootdir=DEFAULT_ROOTDIR)
+    print(f"Building stats from {_tsfile}")
+    with open(_tsfile, "rb") as fin:
         data = pickle.load(fin)
 
     rstats = {}
+    order_strs = {"lo": "low", "true": "matching", "hi": "high"}
     for _order in ("lo", "true", "hi"):
 
         _res = data[f"r{_order}"]
         rstats[_order] = {
-            "file": file,
+            "file": _tsfile,
             "ctrue": data["ctrue"],
             "ztrue": data["ztrue"],
             "zdata": data["zdata"],
@@ -50,14 +68,27 @@ def get_stats(file):
             "rho": np.fft.ifft2(np.abs(np.fft.fft2(_res))**2).real / np.product(_res.shape),
             "cps": np.abs(np.fft.fft2(_res))**2,
         }
+        _res_flattened = _res.flatten()
+        rng.shuffle(_res_flattened)
+        _shuffled_res = _res_flattened.reshape(_res.shape)
+        fig = plt.figure(figsize=FIGSIZE)
+        plt.pcolor(_shuffled_res, cmap=CMAP); plt.colorbar()
+        plt.clim([-2.5, 2.5])
+        _title_str = f"Shuffled {order_strs[_order]} order polynomial residuals"
+        plt.title(_title_str, size=TITLE_SIZE)
+        plt.tight_layout()
+        plt.savefig(os.path.join(_tfolder, f"{_order}_shuffled_{timestamp}.png"))
 
     return tuple(rstats[_o] for _o in ("lo", "true", "hi"))
 
 
 def report_stats(timestamp):
+    """Gets stats for a timestamp, generates additional plots, and stores all
+    useful data to yaml format output in the timstamp folder.
+    """
     _tfolder, _tsfile = pathfile(timestamp, rootdir=DEFAULT_ROOTDIR)
     print(f"Building stats from {_tsfile}")
-    _lo, _true, _hi = get_stats(_tsfile)
+    _lo, _true, _hi = get_stats(timestamp)
     stats = {"lo": _lo, "true": _true, "hi": _hi}
     print("RSS (lo, true, hi):")
     print(tuple(stats[_order]["RSS"] for _order in ("lo", "true", "hi")))
