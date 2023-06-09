f5ebd084f311b4d4c3f8448813458045f234c579
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-06-09 23:09:41 +0100

    refactoring of plotting shuffled residuals into a separate func, consistency edits

--- further_analysis_polynomials_2d.py
+++ further_analysis_polynomials_2d.py
@@ -20,20 +20,25 @@ import matplotlib.pyplot as plt
 import fitting_polynomials_2d
 from fitting_polynomials_2d import PROJDIR
 from fitting_polynomials_2d import FIGSIZE
+from fitting_polynomials_2d import CLIM
 from fitting_polynomials_2d import CMAP
 from fitting_polynomials_2d import TITLE_SIZE
 
 
-# Params
-# ------
+# Parameters
+# ==========
 
-# Gather the timestamps of all results generated so far with the main script in
+# Gather the timestamps of all regression results generated so far with the main script in
 # fitting_polynomials_2d.py
 TIMESTAMPS = [os.path.basename(_p) for _p in glob.glob(os.path.join(PROJDIR, "*-*-*T*"))]
 
+# Degrees of the polynomial series model sets used in the regressions
+DEGREES = ("lo", "true", "hi")
+DEGREE_STRS = {"lo": "low", "true": "matching", "hi": "high"}  # used in display
+
 
 # Functions
-# ---------
+# =========
 
 def pathfile(timestamp, projdir=PROJDIR):
     """Returns the folder and output pickle filename corresponding to a run
@@ -46,29 +51,22 @@ def pathfile(timestamp, projdir=PROJDIR):
     return _tfolder, os.path.join(_tfolder, f"output_{timestamp}.pickle")
 
 
-def get_stats(timestamp, rng=None):
-    """Retrieves, calculates and returns key stats from a
-    fitting_polynomials_2d.py output pickle file for a given timestamp, and
-    generates randomized (shuffled) versions of the residual plots.
-
-    Also generages a PNG image of a shuffled (by input rng if given) residual
-    grid.
+def get_stats(timestamp):
+    """Calculates and returns key stats from a fitting_polynomials_2d.py output
+    pickle file for the given timestamp.
     """
-    if rng is None:
-        rng = np.random.default_rng()
-
     _tfolder, _tsfile = pathfile(timestamp, projdir=PROJDIR)
     print(f"Building stats from {_tsfile}")
     with open(_tsfile, "rb") as fin:
         data = pickle.load(fin)
 
     rstats = {}
-    order_strs = {"lo": "low", "true": "matching", "hi": "high"}
-    for _order in ("lo", "true", "hi"):
+    for _degree in DEGREES:
 
-        _res = data[f"r{_order}"]
-        rstats[_order] = {
+        _res = data[f"r{_degree}"]
+        rstats[_degree] = {
             "file": _tsfile,
+            "timestamp": timestamp,
             "ctrue": data["ctrue"],
             "ztrue": data["ztrue"],
             "zdata": data["zdata"],
@@ -77,39 +75,59 @@ def get_stats(timestamp, rng=None):
             "rho": np.fft.ifft2(np.abs(np.fft.fft2(_res))**2).real / np.product(_res.shape),
             "cps": np.abs(np.fft.fft2(_res))**2,
         }
-        _res_flattened = _res.flatten()
-        rng.shuffle(_res_flattened)
-        _shuffled_res = _res_flattened.reshape(_res.shape)
-        fig = plt.figure(figsize=FIGSIZE)
-        plt.pcolor(_shuffled_res, cmap=CMAP); plt.colorbar()
-        plt.clim([-2.5, 2.5])
-        _title_str = f"Shuffled {order_strs[_order]} order polynomial residuals"
-        plt.title(_title_str, size=TITLE_SIZE)
-        plt.tight_layout()
-        if _order == "true":  # annoyingly fitting_polynomials_2d saved images down as matching_*
-            plt.savefig(os.path.join(_tfolder, f"matching_shuffled_{timestamp}.png"))
-        else:
-            plt.savefig(os.path.join(_tfolder, f"{_order}_shuffled_{timestamp}.png"))
-        plt.close(fig)
 
-    return tuple(rstats[_o] for _o in ("lo", "true", "hi"))
+    return {_degree: rstats[_degree] for _degree in DEGREES}
 
 
-def report_stats(timestamp, rng=None):
-    """Gets stats for a timestamp, generates additional plots, and stores all
-    useful data to yaml format output in the timstamp folder.
+def report_stats(timestamp):
+    """Calls get_stats for an input timestamp, storing the returned dict to yaml
+    format output in the timestamp folder.
     """
     _tfolder, _tsfile = pathfile(timestamp, projdir=PROJDIR)
-    _lo, _true, _hi = get_stats(timestamp, rng=rng)
-    stats = {"lo": _lo, "true": _true, "hi": _hi}
+    stats = get_stats(timestamp)
     print("RSS (lo, true, hi):")
-    print(tuple(stats[_order]["RSS"] for _order in ("lo", "true", "hi")))
+    print(tuple(stats[_degree]["RSS"] for _degree in ("lo", "true", "hi")))
+
     statfile = os.path.join(_tfolder, f"stats_{timestamp}.yaml")
     print(f"Writing to {statfile}")
     with open(statfile, "w") as fout:
         yaml.dump(stats, fout)
-    return
 
+    return stats
+
+
+def plot_shuffled_residuals(stats, rng=None):
+    """Generates a randomly-shuffled image of the all the residuals from an
+    input stats dict.
+    """
+    if rng is None:
+        rng = np.random.default_rng()
+
+    for _degree in DEGREES:
+
+        timestamp = stats[_degree]["timestamp"]
+        _tfolder, _ = pathfile(timestamp, projdir=PROJDIR)
+        res = stats[_degree]["residuals"]
+        # re-order residuals using shuffle
+        res_flattened = res.flatten()
+        rng.shuffle(res_flattened)
+        shuffled_res = res_flattened.reshape(res.shape)
+        # plot
+        fig = plt.figure(figsize=FIGSIZE)
+        plt.pcolor(shuffled_res, cmap=CMAP); plt.colorbar()
+        plt.clim(CLIM)
+        title_str = f"Shuffled {DEGREE_STRS[_degree]} degree polynomial residuals"
+        plt.title(title_str, size=TITLE_SIZE)
+        plt.tight_layout()
+        if _degree == "true":  # annoyingly fitting_polynomials_2d saved images down as matching_*
+            plt.savefig(os.path.join(_tfolder, f"matching_shuffled_{timestamp}.png"))
+        else:
+            plt.savefig(os.path.join(_tfolder, f"{_degree}_shuffled_{timestamp}.png"))
+        plt.close(fig)
+
+
+# Main script
+# ===========
 
 if __name__ == "__main__":
 
@@ -117,4 +135,5 @@ if __name__ == "__main__":
     rng = np.random.default_rng()
     for _timestamp in TIMESTAMPS:
 
-        report_stats(_timestamp, rng=rng)
+        stats = report_stats(_timestamp)
+        plot_shuffled_residuals(stats, rng=rng)
