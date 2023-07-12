4c73f34456e3abaff3831616898c7f37306e1769
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-07-12 21:29:24 +0100

    refactor

--- further_analysis_polynomials_2d.py
+++ further_analysis_polynomials_2d.py
@@ -48,28 +48,34 @@ def pathfile(timestamp, projdir=PROJDIR):
     """Returns the folder and output pickle filename corresponding to a run
     of fitting_polynomials_2d.py for the given timestamp.
     """
-    _tfolder = os.path.join(projdir, timestamp)
-    if not os.path.isdir(_tfolder):
-        raise FileNotFoundError(_tfolder)
+    tfolder = os.path.join(projdir, timestamp)
+    if not os.path.isdir(tfolder):
+        raise FileNotFoundError(tfolder)
 
-    return _tfolder, os.path.join(_tfolder, f"output_{timestamp}.pickle")
+    return tfolder, os.path.join(tfolder, f"output_{timestamp}.pickle")
 
 
-def get_stats(timestamp):
-    """Calculates and returns key stats from a fitting_polynomials_2d.py output
-    pickle file for the given timestamp.
+def get_data_stats(timestamp):
+    """Gets data, then calculates and returns some second order stats from a
+    fitting_polynomials_2d.py output pickle file for the given timestamp.
     """
-    _tfolder, _tsfile = pathfile(timestamp, projdir=PROJDIR)
-    print(f"Building stats from {_tsfile}")
-    with open(_tsfile, "rb") as fin:
+    tfolder, tsfile = pathfile(timestamp, projdir=PROJDIR)
+    print(f"Building stats from {tsfile}")
+    with open(tsfile, "rb") as fin:
         data = pickle.load(fin)
+
+    # For later convenience, add path information to the data dict
+    data["file"] = tsfile
+    data["folder"] = tfolder
+    data["timestamp"] = timestamp
 
     rstats = {}
     for _degree in DEGREES:
 
         _res = data[f"r{_degree}"]
         rstats[_degree] = {
-            "file": _tsfile,
+            "file": tsfile,
+            "folder": tfolder,
             "timestamp": timestamp,
             "ctrue": data["ctrue"],
             "ztrue": data["ztrue"],
@@ -80,24 +86,24 @@ def get_stats(timestamp):
             "cps": np.abs(np.fft.fft2(_res))**2,
         }
 
-    return {_degree: rstats[_degree] for _degree in DEGREES}
+    return data, {_degree: rstats[_degree] for _degree in DEGREES}
 
 
 def report_stats(timestamp):
-    """Calls get_stats for an input timestamp, storing the returned dict to yaml
-    format output in the timestamp folder.
+    """Calls get_data_stats for an input timestamp, storing the returned stats
+    dict to yaml format output in the timestamp folder, and returning the
+    (data, stats) tuple
     """
-    _tfolder, _tsfile = pathfile(timestamp, projdir=PROJDIR)
-    stats = get_stats(timestamp)
+    data, stats = get_data_stats(timestamp)
     print("RSS (lo, true, hi, vhi):")
     print(tuple(stats[_degree]["RSS"] for _degree in DEGREES))
 
-    statfile = os.path.join(_tfolder, f"stats_{timestamp}.yaml")
+    statfile = os.path.join(data["folder"], f"stats_{timestamp}.yaml")
     print(f"Writing to {statfile}")
     with open(statfile, "w") as fout:
         yaml.dump(stats, fout)
 
-    return stats
+    return data, stats
 
 
 def plot_shuffled_residuals(stats, rng=None):
@@ -109,8 +115,6 @@ def plot_shuffled_residuals(stats, rng=None):
 
     for _degree in DEGREES:
 
-        timestamp = stats[_degree]["timestamp"]
-        _tfolder, _ = pathfile(timestamp, projdir=PROJDIR)
         res = stats[_degree]["residuals"]
         # re-order residuals using shuffle
         res_flattened = res.flatten()
@@ -118,15 +122,18 @@ def plot_shuffled_residuals(stats, rng=None):
         shuffled_res = res_flattened.reshape(res.shape)
         # plot
         fig = plt.figure(figsize=FIGSIZE)
-        plt.pcolor(shuffled_res, cmap=CMAP); plt.colorbar()
+        plt.pcolor(shuffled_res, cmap=CMAP)
+        plt.colorbar()
         plt.clim(CLIM)
         title_str = f"Shuffled {DEGREE_STRS[_degree]} degree polynomial residuals"
         plt.title(title_str, size=TITLE_SIZE)
         plt.tight_layout()
+        timestamp = stats[_degree]["timestamp"]
         if _degree == "true":  # annoyingly fitting_polynomials_2d saved images down as matching_*
-            plt.savefig(os.path.join(_tfolder, f"matching_shuffled_{timestamp}.png"))
+            outfile = os.path.join(stats[_degree]["folder"], f"matching_shuffled_{timestamp}.png")
         else:
-            plt.savefig(os.path.join(_tfolder, f"{_degree}_shuffled_{timestamp}.png"))
+            outfile =  os.path.join(stats[_degree]["folder"], f"{_degree}_shuffled_{timestamp}.png")
+        plt.savefig(outfile)
         plt.close(fig)
 
 
@@ -136,8 +143,7 @@ def plot_histogram_residuals(stats):
     """
     for _degree in DEGREES:
 
-        timestamp = stats[_degree]["timestamp"]
-        _tfolder, _ = pathfile(timestamp, projdir=PROJDIR)
+        print(f"Plotting {_degree} histogram")
         fig = plt.figure(figsize=FIGSIZE)
         plt.hist(
             stats[_degree]["residuals"].flatten(), bins=HIST_NBINS, range=HIST_RANGE, color="Gray")
@@ -148,36 +154,35 @@ def plot_histogram_residuals(stats):
         title_str = f"Histogram of {DEGREE_STRS[_degree]} degree polynomial residuals"
         plt.title(title_str, size=TITLE_SIZE)
         plt.tight_layout()
+        timestamp = stats[_degree]["timestamp"]
         if _degree == "true":  # annoyingly fitting_polynomials_2d saved images down as matching_*
-            plt.savefig(os.path.join(_tfolder, f"hist_matching_{timestamp}.png"))
+            outfile = os.path.join(stats[_degree]["folder"], f"hist_matching_{timestamp}.png")
         else:
-            plt.savefig(os.path.join(_tfolder, f"hist_{_degree}_{timestamp}.png"))
+            outfile = os.path.join(stats[_degree]["folder"], f"hist_{_degree}_{timestamp}.png")
+        plt.savefig(outfile)
         plt.close(fig)
 
 
-def plot_predictions(timestamp):
+def plot_predictions(data):
     """Generates plots of predictions from fitting_polynomials_2d.py output
     and saves it into the same folder for the given timestamp.
     """
-    _tfolder, _tsfile = pathfile(timestamp, projdir=PROJDIR)
-    with open(_tsfile, "rb") as fin:
-        data = pickle.load(fin)
-
     for _degree in DEGREES:
 
-        timestamp = stats[_degree]["timestamp"]
-        _tfolder, _ = pathfile(timestamp, projdir=PROJDIR)
+        print(f"Plotting {_degree} prediction")
         pred = data["pred_"+_degree]
         # plot
         fig = plt.figure(figsize=FIGSIZE)
-        plt.pcolor(pred, cmap=CMAP); plt.colorbar()
+        plt.pcolor(pred, cmap=CMAP)
+        plt.colorbar()
         title_str = f"{DEGREE_STRS[_degree].title()} degree polynomial prediction"
         plt.title(title_str, size=TITLE_SIZE)
         plt.tight_layout()
+        timestamp = data["timestamp"]
         if _degree == "true":  # annoyingly fitting_polynomials_2d saved images down as matching_*
-            plt.savefig(os.path.join(_tfolder, f"matching_prediction_{timestamp}.png"))
+            plt.savefig(os.path.join(data["folder"], f"matching_prediction_{timestamp}.png"))
         else:
-            plt.savefig(os.path.join(_tfolder, f"{_degree}_prediction_{timestamp}.png"))
+            plt.savefig(os.path.join(data["folder"], f"{_degree}_prediction_{timestamp}.png"))
         plt.close(fig)
 
 
@@ -186,11 +191,9 @@ def plot_predictions(timestamp):
 
 if __name__ == "__main__":
 
-    # Loop through timestamps and report stats into folders
     rng = np.random.default_rng()
     for _timestamp in TIMESTAMPS:
 
-        stats = report_stats(_timestamp)
-        plot_predictions(_timestamp)
+        data, stats = report_stats(_timestamp)
         plot_shuffled_residuals(stats, rng=rng)
         plot_histogram_residuals(stats)
