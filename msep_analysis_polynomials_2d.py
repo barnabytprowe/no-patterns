7df29bd59906e47201cd36af447a2c5cc7216685
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-07-12 22:44:52 +0100

    make a start on bigger simulation script

---
+++ msep_analysis_polynomials_2d.py
@@ -0,0 +1,25 @@
+"""
+msep_analysis_polynomials_2d.py
+===============================
+
+Mean Squared Errors of Prediction analysis.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.linear_model
+import sklearn.preprocessing
+
+import fitting_polynomials_2d
+
+
+# Parameters
+# ==========
+
+NRUNS = 100
+
+
+# Main script
+# ===========
+
+if __name__ == "__main__":
