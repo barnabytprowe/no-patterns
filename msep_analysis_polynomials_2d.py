f8132a4c5d450ecfa35965a595da094f63da6791
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2023-07-14 12:17:58 +0100

    generate the design matrices, ideal coeffs and model images

--- msep_analysis_polynomials_2d.py
+++ msep_analysis_polynomials_2d.py
@@ -16,10 +16,48 @@ import fitting_polynomials_2d
 # Parameters
 # ==========
 
-NRUNS = 100
+NRUNS = 1000
 
 
 # Main script
 # ===========
 
 if __name__ == "__main__":
+
+    rng = np.random.default_rng()
+
+    # Design matrices
+    features_lo = fitting_polynomials_2d.polynomial_design_matrix(
+        square_dimension=fitting_polynomials_2d.side_dim,
+        n_side=fitting_polynomials_2d.nx,
+        degree=fitting_polynomials_2d.fit_degree_lo,
+        order="C",
+    )
+    features_true = fitting_polynomials_2d.polynomial_design_matrix(
+        square_dimension=fitting_polynomials_2d.side_dim,
+        n_side=fitting_polynomials_2d.nx,
+        degree=fitting_polynomials_2d.fit_degree_true,
+        order="C",
+    )
+    features_hi = fitting_polynomials_2d.polynomial_design_matrix(
+        square_dimension=fitting_polynomials_2d.side_dim,
+        n_side=fitting_polynomials_2d.nx,
+        degree=fitting_polynomials_2d.fit_degree_hi,
+        order="C",
+    )
+    features_vhi = fitting_polynomials_2d.polynomial_design_matrix(
+        square_dimension=fitting_polynomials_2d.side_dim,
+        n_side=fitting_polynomials_2d.nx,
+        degree=fitting_polynomials_2d.fit_degree_vhi,
+        order="C",
+    )
+
+    # Ideal model coefficients and corresponding images on the coordinate grid
+    ctrue = rng.normal(
+        loc=0.,
+        scale=fitting_polynomials_2d.coeff_signal_to_noise,
+        size=(NRUNS, features_true.shape[-1]),
+    )
+    ztrue = (
+        np.matmul(features_true, ctrue.T).T
+    ).reshape(NRUNS, fitting_polynomials_2d.nx, fitting_polynomials_2d.nx, order="C")
