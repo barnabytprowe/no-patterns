13c0e03e1f4315fb9bd906d22f5662983d58e1f3
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2024-05-07 00:46:04 +0100

    new design matrix calc and plotting

--- fitting_polynomials_2d.py
+++ fitting_polynomials_2d.py
@@ -57,26 +57,6 @@ PROJDIR = os.path.join(PLTDIR, "polynomials_2d")
 # Functions
 # =========
 
-def polynomial_design_matrix(square_dimension, n_side, degree, order="C"):
-    """Returns a polynomial design matrix for a square coordinate grid centred
-    on the origin, up to the given degree in total polynomial order.
-
-    Args:
-        square_dimension: side dimensions of the coordinate grid square
-        n_side: number of grid points per side
-        degree: maximum polynomial degree of features in all dimensions
-        order: array row/column ordering to use in np.flatten, default: 'C'
-    """
-    # Define x, y grid coords for square of linear dimension side_dim centred on the origin
-    xvals = np.linspace(-square_dimension / 2., square_dimension / 2., num=n_side, endpoint=True)
-    Xxgrid, Xygrid = np.meshgrid(xvals, xvals)
-    X = np.stack([Xxgrid.flatten(order=order), Xygrid.flatten(order=order)], axis=1)
-
-    # Use sklearn PolynomialFeatures to model simple polynomials over x0, x1 coordinates
-    poly_features = sklearn.preprocessing.PolynomialFeatures(degree=degree)
-    return poly_features.fit_transform(X, y=None)
-
-
 def build_output_folder_structure(timestamp, project_dir=PROJDIR):
     """Builds output folder structure using input timestamp and module scope
     PLTDIR, PROJDIR constant variables.  Returns output folder name.
@@ -93,6 +73,78 @@ def build_output_folder_structure(timestamp, project_dir=PROJDIR):
     return outdir
 
 
+# Consistent functions for determining p, q indices labels for the cth
+# coefficient theta_pq, used in the construction of the design matrix
+
+def _dtri(c):
+    """Total order (in both x0 and x1) of the cth column polynomial minus one"""
+    return np.floor(.5 * (np.sqrt(1. + 8. * c) - 3)).astype(int)
+
+
+def _ntri(c):
+    """Number of coefficients in the complete series of degree _dtri"""
+    return (1 + _dtri(c)) * (2 + _dtri(c)) // 2
+
+
+def _ps(c):
+    """p index label for the cth coefficient theta_pq"""
+    return 1 + _dtri(c) + _ntri(c) - c
+
+
+def _qs(c):
+    """q index label for the cth coefficient theta_pq"""
+    return c - _ntri(c)
+
+
+def chebyshev_design_matrix(x0, x1, degree):
+    """Returns the Chebyshev polynomial design matrix up to input degree for two
+    independent coordinates x0, x1
+    """
+    if len(x0) != len(x1):
+        raise ValueError("input coordinate arrays x0 and x1 unequal length")
+
+    # Get the columns of the matrix corresponding to the x0, x1 coordinate grids
+    i1n = np.eye(1 + degree)
+    mp = np.asarray([np.polynomial.chebyshev.chebval(x0, _row) for _row in i1n]).T
+    mq = np.asarray([np.polynomial.chebyshev.chebval(x1, _row) for _row in i1n]).T
+
+    # Prepare final output matrix
+    ncoeff = (degree + 1) * (degree + 2) // 2
+    ps = _ps(np.arange(ncoeff, dtype=int))
+    qs = _qs(np.arange(ncoeff, dtype=int))
+    design_matrix = np.concatenate(
+        [(mp[:, _p] * mq[:, _q]).reshape(len(x0), 1) for _p, _q in zip(ps, qs)],
+        axis=1,
+    )
+    return design_matrix
+
+
+def plot_image(data, title, filename=None, clim=None, show=True, tick_stride=7):
+    """Quick image plot using module level constant settings"""
+
+    fig = plt.figure(figsize=FIGSIZE)
+    plt.title(title, size=TITLE_SIZE)
+    plt.pcolor(data, cmap=CMAP)
+    if clim is not None:
+        plt.clim(clim)
+    plt.colorbar()
+
+    # Ensure integer ticks
+    xticks = np.linspace(
+        0, data.shape[0], num=1 + data.shape[0]//tick_stride, endpoint=True, dtype=int)
+    yticks = np.linspace(
+        0, data.shape[1], num=1 + data.shape[1]//tick_stride, endpoint=True, dtype=int)
+    plt.xticks(xticks)
+    plt.yticks(yticks)
+    plt.tight_layout()
+
+    if filename is not None:
+        plt.savefig(filename)
+    if show:
+        plt.show()
+    return
+
+
 # Main script
 # ===========
 
@@ -105,50 +157,39 @@ if __name__ == "__main__":
     # Output dict - will be pickled
     output = {}
 
+    # Prepare two independent variables on a grid
+    xvals = np.linspace(x0x1_min, x0x1_max, num=nx, endpoint=True)
+    x0, x1 = np.meshgrid(xvals, xvals)
+    x0 = x0.flatten(order="C")
+    x1 = x1.flatten(order="C")
+
     # Design matrices
-    features_fit_lo = polynomial_design_matrix(
-        square_dimension=side_dim, n_side=nx, degree=fit_degree_lo, order="C")
-    features_true = polynomial_design_matrix(
-        square_dimension=side_dim, n_side=nx, degree=fit_degree_true, order="C")
-    features_fit_hi = polynomial_design_matrix(
-        square_dimension=side_dim, n_side=nx, degree=fit_degree_hi, order="C")
-    features_fit_vhi = polynomial_design_matrix(
-        square_dimension=side_dim, n_side=nx, degree=fit_degree_vhi, order="C")
+    design_lo = chebyshev_design_matrix(x0, x1, degree=fit_degree_lo)
+    design_true = chebyshev_design_matrix(x0, x1, degree=fit_degree_true)
+    design_hi = chebyshev_design_matrix(x0, x1, degree=fit_degree_hi)
+    design_vhi = chebyshev_design_matrix(x0, x1, degree=fit_degree_vhi)
 
     # Build the true / ideal 2D contour and plot
-    ctrue = np.random.randn(features_true.shape[-1]) * coeff_signal_to_noise
-    ztrue = (np.matmul(features_true, ctrue)).reshape((nx, nx), order="C")
+    ctrue = np.random.randn(design_true.shape[-1]) * coeff_signal_to_noise
+    ztrue = (np.matmul(design_true, ctrue)).reshape((nx, nx), order="C")
+    plot_image(ztrue, "Ideal model", filename=os.path.join(outdir, "ideal_"+tstmp+".png"))
     output["ctrue"] = ctrue
     output["ztrue"] = ztrue
 
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.title("Ideal model curve", size=TITLE_SIZE)
-    plt.pcolor(ztrue, cmap=CMAP)
-    plt.colorbar()
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "ideal_"+tstmp+".png"))
-    plt.show()
-
     # Add the random noise to generate the dataset and plot
     zdata = ztrue + noise_sigma * np.random.randn(*ztrue.shape)
+    plot_image(zdata, "Data", filename=os.path.join(outdir, "data_"+tstmp+".png"))
     output["zdata"] = zdata
-
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.title("Data", size=TITLE_SIZE)
-    plt.pcolor(zdata, cmap=CMAP)
-    plt.colorbar()
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "data_"+tstmp+".png"))
-    plt.show()
 
     # Perform too low, matching, too high, and very much too high degree regressions on data
     regr = sklearn.linear_model.LinearRegression()  # uses LAPACK via np leastsq under the hood
     zflat = zdata.flatten(order="C")
     predictions = []
-    for features in (features_fit_lo, features_true, features_fit_hi, features_fit_vhi):
+    for _design in (design_lo, design_true, design_hi, design_vhi):
 
-        regr.fit(features, zflat)
-        predictions.append(regr.predict(features).reshape((nx, nx), order="C"))
+        _coeffs = np.linalg.lstsq(_design, zdata.flatten(order="C"), rcond=None)[0]
+        _prediction = _design.dot(_coeffs.T).reshape((nx, nx), order="C")
+        predictions.append(_prediction)
 
     pred_lo, pred_true, pred_hi, pred_vhi = tuple(predictions)
     output["pred_lo"] = pred_lo
@@ -158,50 +199,37 @@ if __name__ == "__main__":
 
     # Residuals
     rlo = zdata - pred_lo
+    plot_image(
+        rlo,
+        "Low degree polynomial residuals",
+        filename=os.path.join(outdir, "lo_"+tstmp+".png"),
+        clim=CLIM,
+    )
     rtrue = zdata - pred_true
+    plot_image(
+        rtrue,
+        "Matching degree polynomial residuals",
+        filename=os.path.join(outdir, "matching_"+tstmp+".png"),
+        clim=CLIM,
+    )
     rhi = zdata - pred_hi
+    plot_image(
+        rhi,
+        "High degree polynomial residuals",
+        filename=os.path.join(outdir, "hi_"+tstmp+".png"),
+        clim=CLIM,
+    )
     rvhi = zdata - pred_vhi
+    plot_image(
+        rvhi,
+        "Very high degree polynomial residuals",
+        filename=os.path.join(outdir, "vhi_"+tstmp+".png"),
+        clim=CLIM,
+    )
     output["rlo"] = rlo
     output["rtrue"] = rtrue
     output["rhi"] = rhi
     output["rvhi"] = rvhi
-
-    # Plot residuals
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.pcolor(rlo, cmap=CMAP)
-    plt.colorbar()
-    plt.clim(CLIM)
-    plt.title("Low degree polynomial residuals", size=TITLE_SIZE)
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "lo_"+tstmp+".png"))
-    plt.show()
-
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.pcolor(rtrue, cmap=CMAP)
-    plt.colorbar()
-    plt.clim(CLIM)
-    plt.title("Matching degree polynomial residuals", size=TITLE_SIZE)
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "matching_"+tstmp+".png"))
-    plt.show()
-
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.pcolor(rhi, cmap=CMAP)
-    plt.colorbar()
-    plt.clim(CLIM)
-    plt.title("High degree polynomial residuals", size=TITLE_SIZE)
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "hi_"+tstmp+".png"))
-    plt.show()
-
-    fig = plt.figure(figsize=FIGSIZE)
-    plt.pcolor(rvhi, cmap=CMAP)
-    plt.colorbar()
-    plt.clim(CLIM)
-    plt.title("Very high degree polynomial residuals", size=TITLE_SIZE)
-    plt.tight_layout()
-    plt.savefig(os.path.join(outdir, "vhi_"+tstmp+".png"))
-    plt.show()
 
     # Save output for further analysis
     outfile = os.path.join(outdir, "output_"+tstmp+".pickle")
