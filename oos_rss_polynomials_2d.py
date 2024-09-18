acc9a3e692b99fcad03c24974bab599796cf6d02
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2024-09-18 17:35:04 +0100

    port more over

--- oos_rss_polynomials_2d.py
+++ oos_rss_polynomials_2d.py
@@ -41,12 +41,77 @@ if NRUNS not in NRUNS_STRS:
 NCORES = 2
 
 # Gather module scope degrees into an array for convenience, use values from fitting_polynomials_2d
-degree_values = (
+_dvals = (
     fitting_polynomials_2d.fit_degree_lo,
     fitting_polynomials_2d.fit_degree_true,
     fitting_polynomials_2d.fit_degree_hi,
     fitting_polynomials_2d.fit_degree_vhi,
 )
-degree_titles = {
-    _d: DEGREE_STRS[_d].title()+" (G="+str(_dv)+")" for _d, _dv in zip(DEGREES, degree_values)
+DEGREE_VALS = {
+    _d: _dval for _d, _dv in zip(DEGREES, _dvals)
 }
+DEGREE_TITLES = {
+    _d: DEGREE_STRS[_d].title()+" (G="+str(_dval)+")" for _d, _dv in zip(DEGREES, _dvals)
+}
+
+
+# Functions
+# =========
+
+def _fit_predict(data_flat, design_matrix=None, nx=fitting_polynomials_2d.nx, order="C"):
+    """Perform regression on input fitting_polynomial_2d.py-style dataset using
+    input features, returning regression prediction
+    """
+    coeffs = np.linalg.lstsq(design_matrix, data_flat, rcond=None)[0].T
+    return design_matrix.dot(coeffs).reshape((nx, nx), order=order)
+
+
+def build_regression_sample(
+    rng, degree_vals=DEGREE_VALS, nx=fitting_polynomials_2d.nx, nruns=NRUNS
+):
+    """Run full large sample analysis and return results in a dictionary"""
+    output = {}
+    feature_labels = {_d: f"features_{_d}" for _d in degree_vals}
+
+    # Prepare two independent variables on a grid
+    xvals = np.linspace(x0x1_min, x0x1_max, num=nx, endpoint=True)
+    x0, x1 = np.meshgrid(xvals, xvals)
+    x0 = x0.flatten(order="C")
+    x1 = x1.flatten(order="C")
+
+    # Design matrices
+    for _d in degree_vals:
+
+        output[feature_labels[_d]] = fitting_polynomials_2d.chebyshev_design_matrix(
+            x0, x1, degree=degree_values[_d])
+
+    # Ideal model coefficients and corresponding images on the coordinate grid
+    print("Generating ideal model coefficients")
+    output["ctrue"] = rng.normal(
+        loc=0., scale=coeff_signal_to_noise, size=(nruns, output["features_true"].shape[-1]))
+    output["ztrue"] = (
+        np.matmul(output["features_true"], output["ctrue"].T).T).reshape((nruns, nx, nx), order="C")
+
+    # Generate the errors we will add to create simulated data
+    print("Generating data errors")
+    output["errors"] = rng.normal(loc=0., scale=noise_sigma, size=(nruns, nx, nx))
+    output["zdata"] = output["ztrue"] + output["errors"]
+
+    oshape = output["zdata"].shape
+    # Perform too low, matching, too high, and very much too high degree regressions on data
+    zdata_flat = output["zdata"].reshape(oshape[0], oshape[1] * oshape[2], order="C")
+    output["predictions"] = {}
+    for _dstr, _design_matrix in zip(
+        degree_vals.keys(), [output[_flab] for _flab in feature_labels]
+    ):
+        _pfunc = functools.partial(_fit_predict, design_matrix=_design_matrix)
+        print(f"Regressing {nruns} {_dstr} runs using {NCORES} cores")
+        with multiprocessing.Pool(NCORES) as p:
+            output["predictions"][_dstr] = np.asarray(
+                p.map(_pfunc, [_zf for _zf in zdata_flat]), dtype=float)
+
+    # Generate a new set of errors from which to calculate the Mean Squared Error of Prediction
+    print("Generating new data errors")
+    output["errors_new"] = rng.normal(loc=0., scale=noise_sigma, size=(nruns, nx, nx))
+    output["zdata_new"] = output["ztrue"] + output["errors_new"]
+    return output
