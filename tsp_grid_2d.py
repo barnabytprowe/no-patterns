6f83c3efeda7f53a678fcbb2481ed3f59152bd6c
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2025-03-30 02:17:46 +0100

    rework 2opt to make simpler using starmap

--- tsp_grid_2d.py
+++ tsp_grid_2d.py
@@ -6,6 +6,7 @@ grids
 import multiprocessing
 import pickle
 import time
+from itertools import repeat
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -17,13 +18,14 @@ from python_tsp.heuristics import solve_tsp_lin_kernighan, solve_tsp_local_searc
 import fitting_polynomials_2d
 
 
-# Number of processes
-NPROC = 7
+# Number of processes to run simultaneously, and total runs to attempt
+NPROC = 10
+NRUNS = 30
 
 INITIALIZE_WITH_CHRISTOFIDES = True
 # Timeout settings (all in seconds)
 # 2-opt initial optimization
-TIMEOUT_2OPT = 1200
+TIMEOUT_2OPT = 300
 # Lin-Kernighan
 CYCLE_LK = 1  # checks for completion every cycle
 TIMEOUT_LK = 30  # total timeout
@@ -33,6 +35,18 @@ ngrid = 28
 
 # Pickle file output
 output_pickle = f"tsp_grid_2d_{ngrid}x{ngrid}_{NPROC}_{TIMEOUT_2OPT}.pkl"
+
+
+# Pool starmap utilities
+
+def starmap_with_kwargs(pool, fn, args_iter, kwargs_iter):
+    args_for_starmap = zip(repeat(fn), args_iter, kwargs_iter)
+    return pool.starmap(apply_args_and_kwargs, args_for_starmap)
+
+
+def apply_args_and_kwargs(fn, args, kwargs):
+    return fn(*args, **kwargs)
+
 
 
 def distance_matrix(xy):
@@ -93,42 +107,6 @@ def plot_path(xy, path, figsize=(6, 5.5), title=None):
     return ax
 
 
-def process_local_search(
-    pk,
-    results_dict,
-    distance_matrix=None,
-    x0=None,
-    max_processing_time=None,
-    perturbation_scheme="two_opt",
-):
-    """Target function for multiprocessing approximate TSP solutions using local
-    search heuristics.
-
-    Stores output of call to python_tsp.heuristics.solve_tsp_local_search in
-    results_dict[pk].
-
-    Args:
-        pk: process key for result storage
-        results_dict:
-            dict-like as returned by multiprocessing.Manager().dict(), to be
-            used for shared memory storage of solution results
-        distance_matrix: passed to solve_tsp_local_search
-        x0: start path, if any, passed to solve_tsp_local_search
-        max_processing_time: passed to solve_tsp_local_search
-        perturbation_scheme: passed to solve_tsp_local_search
-    """
-    if distance_matrix is None:
-        raise ValueError("distance_matrix must be supplied")
-
-    results_dict[pk] = solve_tsp_local_search(
-        distance_matrix=distance_matrix,
-        x0=x0,
-        perturbation_scheme=perturbation_scheme,
-        max_processing_time=max_processing_time,
-        verbose=False,
-    )
-
-
 def process_lin_kernighan(pk, results_dict, distance_matrix=None, x0=None):
     """Target function for multiprocessing approximate TSP solutions using the
     Lin-Kernighan algorithm.
@@ -155,7 +133,8 @@ def process_lin_kernighan(pk, results_dict, distance_matrix=None, x0=None):
 
 
 def multiprocess_local_search(
-    dm,
+    distance_matrix,
+    nruns,
     x0=None,
     max_processing_time=TIMEOUT_2OPT,
     nproc=NPROC,
@@ -165,40 +144,48 @@ def multiprocess_local_search(
     solutions using local search heuristics
 
     Args:
-        distance_matrix: passed to python_tsp.heuristics.process_local_search
+        distance_matrix: passed to python_tsp.heuristics.solve_tsp_local_search
+        nruns:
+            number of runs to attempt in total, probably some multiple of nproc
         x0:
-            if set, a length nproc iterable of previous TSP path approximations
+            if set, a length nruns iterable of previous TSP path approximations
             to use as starting points for local search, an element of which is
-            passed to python_tsp.heuristics.process_local_search for each run
-        max_processing_time: passed to python_tsp.heuristics.process_local_search
+            passed to python_tsp.heuristics.solve_tsp_local_search for each run
+        max_processing_time:
+            passed to python_tsp.heuristics.solve_tsp_local_search
         nproc: int number of processes to run simultaneously via multiprocessing
-        perturbation_scheme: passed to python_tsp.heuristics.process_local_search
+        perturbation_scheme:
+            passed to python_tsp.heuristics.solve_tsp_local_search
 
     Returns:
-        dict of returns from python_tsp.heuristics.process_local_search keyed by
+        dict of returns from python_tsp.heuristics.solve_tsp_local_search keyed by
         integer process label, one of range(nproc)
     """
     processes = {}
     manager = multiprocessing.Manager()
     results_storage = manager.dict()
     if x0 is None:
-        x0 = [None] * nproc
+        x0 = [None] * nruns
 
-    for ip in range(nproc):
-        print(f"Launching process {ip} for {perturbation_scheme} TSP, {max_processing_time=}s")
-        processes[ip] = multiprocessing.Process(
-            target=process_local_search,
-            args=(ip, results_storage),
-            kwargs={
-                "distance_matrix": dm, "x0": x0[ip], "max_processing_time": max_processing_time
-            },
+    with multiprocessing.Pool(nproc) as pool:
+        print(f"Launching {nruns} runs for {perturbation_scheme} TSP, {max_processing_time=}s")
+        results = starmap_with_kwargs(
+            pool,
+            solve_tsp_local_search,
+            repeat([]),
+            [
+                {
+                    "distance_matrix": distance_matrix,
+                    "x0": x0[_p],
+                    "perturbation_scheme": perturbation_scheme,
+                    "max_processing_time": max_processing_time,
+                    "verbose": False,
+                }
+                for _p in range(nruns)
+            ],
         )
-        processes[ip].start()
 
-    for ip in range(nproc):
-        processes[ip].join(max_processing_time)
-
-    return results_storage
+    return {_p: _result for _p, _result in enumerate(results)}
 
 
 def multiprocess_lk(dm, x0=None, max_processing_time=TIMEOUT_LK, nproc=NPROC):
@@ -292,8 +279,9 @@ if __name__ == "__main__":
 
     # Solve local (2-opt)
     results_2opt = multiprocess_local_search(
-        dm=dm,
-        x0=[p0] * NPROC,
+        distance_matrix=dm,
+        nruns=NRUNS,
+        x0=[p0] * NRUNS,
         max_processing_time=TIMEOUT_2OPT,
         nproc=NPROC,
         perturbation_scheme="two_opt",
@@ -304,6 +292,8 @@ if __name__ == "__main__":
     odict["total_weights_2opt"] = total_weights_2opt
     print("Total weights after two_opt local search:")
     print(pd.Series(total_weights_2opt))
+
+    import ipdb; ipdb.set_trace()
 
     # Refine with Lin-Kernighan
     results_lk = multiprocess_lk(
