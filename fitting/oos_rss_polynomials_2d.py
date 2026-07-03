97fc532cc51159bc85730beed7fb472b44f0e481
barnaby_rowe <barnaby.t.p.rowe@gmail.com>
2026-07-03 13:01:53 +0100

    continue standardization, happy

--- fitting/oos_rss_polynomials_2d.py
+++ fitting/oos_rss_polynomials_2d.py
@@ -23,8 +23,10 @@ import numpy as np
 import pandas as pd
 
 import polynomials_2d
-from polynomials_2d import PROJDIR
-from further_analysis_polynomials_2d import DEGREES, DEGREE_STRS
+from polynomials_2d import (
+    NX, FIT_DEGREES, X0X1_MIN, X0X1_MAX, NOISE_SIGMA, COEFF_SIGNAL_TO_NOISE, PROJDIR
+)
+from further_analysis_polynomials_2d import DEGREE_STRS
 
 
 # Parameters
@@ -38,23 +40,14 @@ NRUNS = 10000
 # laptop) works best, perhaps due to some under-the-hood parallelization
 NCORES = 2
 
-# Gather module scope degrees into an array for convenience, use values from fitting_polynomials_2d
-_dvals = (
-    polynomials_2d.fit_degree_lo,
-    polynomials_2d.fit_degree_true,
-    polynomials_2d.fit_degree_hi,
-    polynomials_2d.fit_degree_vhi,
-)
-DEGREE_VALS = {
-    _d: _dval for _d, _dval in zip(DEGREES, _dvals)
-}
+# Gather fit degrees and construct display titles
 DEGREE_TITLES = {
     _d: (
         DEGREE_STRS[_d].title()+f"\n$(G={_dval})$"
         if _d != "true"
         else DEGREE_STRS[_d].title()+f"\n$(G = G^* = {_dval})$"
     )
-    for _d, _dval in DEGREE_VALS.items()
+    for _d, _dval in FIT_DEGREES.items()
 }
 
 # Pickle cache file location setup
@@ -79,7 +72,7 @@ MR_OUTFILE = os.path.join(PROJDIR, f"matched-relative_rss_n{NRUNS}.pdf")
 # Functions
 # =========
 
-def _fit_predict(data_flat, design_matrix=None, nx=polynomials_2d.nx, order="C"):
+def _fit_predict(data_flat, design_matrix, nx=NX, order="C"):
     """Perform regression on input fitting_polynomial_2d.py-style dataset using
     input features, returning regression prediction
     """
@@ -89,17 +82,16 @@ def _fit_predict(data_flat, design_matrix=None, nx=polynomials_2d.nx, order="C")
 
 def build_regression_sample(
     rng,
-    degree_vals=DEGREE_VALS,
-    nx=polynomials_2d.nx,
     nruns=NRUNS,
-    x0x1_min=polynomials_2d.x0x1_min,
-    x0x1_max=polynomials_2d.x0x1_max,
-    coeff_signal_to_noise=polynomials_2d.coeff_signal_to_noise,
-    noise_sigma=polynomials_2d.noise_sigma,
+    fit_degrees=FIT_DEGREES,
+    nx=NX,
+    x0x1_min=X0X1_MIN,
+    x0x1_max=X0X1_MIN,
+    noise_sigma=NOISE_SIGMA,
+    coeff_signal_to_noise=COEFF_SIGNAL_TO_NOISE,
 ):
     """Run full large sample analysis and return results in a dictionary"""
     output = {}
-    feature_labels = {_d: f"features_{_d}" for _d in degree_vals}
 
     # Prepare two independent variables on a grid
     xvals = np.linspace(x0x1_min, x0x1_max, num=nx, endpoint=True)
@@ -108,20 +100,19 @@ def build_regression_sample(
     x1 = x1.flatten(order="C")
 
     # Design matrices
-    for _d in degree_vals:
-
-        output[feature_labels[_d]] = polynomials_2d.chebyshev_design_matrix(
-            x0, x1, degree=degree_vals[_d])
+    for _d, _dval in fit_degrees.items():
+        output[f"features_{_d}"] = polynomials_2d.chebyshev_design_matrix(x0, x1, degree=_dval)
 
     # Ideal model coefficients and corresponding images on the coordinate grid
     print("Generating ideal model coefficients")
     output["ctrue"] = rng.normal(
         loc=0.,
-        scale=coeff_signal_to_noise * noise_sigma,
+        scale=noise_sigma * coeff_signal_to_noise,
         size=(nruns, output["features_true"].shape[-1]),
     )
     output["ztrue"] = (
-        np.matmul(output["features_true"], output["ctrue"].T).T).reshape((nruns, nx, nx), order="C")
+        np.matmul(output["features_true"], output["ctrue"].T).T).reshape((nruns, nx, nx), order="C"
+    )
 
     # Generate the errors we will add to create simulated data
     print("Generating data errors")
@@ -131,15 +122,14 @@ def build_regression_sample(
     oshape = output["zdata"].shape
     # Perform too low, matching, too high, and very much too high degree regressions on data
     zdata_flat = output["zdata"].reshape(oshape[0], oshape[1] * oshape[2], order="C")
-    output["predictions"] = {}
-    for _d in degree_vals:
-
-        _design_matrix = output[feature_labels[_d]]
+    for _d in fit_degrees:
+        _design_matrix = output[f"features_{_d}"]
         _pfunc = functools.partial(_fit_predict, design_matrix=_design_matrix, nx=nx, order="C")
         print(f"Regressing {nruns} {_d} runs using multiprocessing with {NCORES=}")
         with multiprocessing.Pool(NCORES) as p:
-            output["predictions"][_d] = np.asarray(
-                p.map(_pfunc, [_zf for _zf in zdata_flat]), dtype=float)
+            output[f"pred_{_d}"] = np.asarray(
+                p.map(_pfunc, [_zf for _zf in zdata_flat]), dtype=float
+            )
 
     # Generate a new set of errors from which to calculate the Mean Squared Error of Prediction
     print("Generating new data errors")
@@ -163,13 +153,13 @@ if __name__ == "__main__":
         t0 = time.time()
         results = build_regression_sample(
             rng,
-            degree_vals=DEGREE_VALS,
-            nx=polynomials_2d.nx,
             nruns=NRUNS,
-            x0x1_min=polynomials_2d.x0x1_min,
-            x0x1_max=polynomials_2d.x0x1_max,
-            coeff_signal_to_noise=polynomials_2d.coeff_signal_to_noise,
-            noise_sigma=polynomials_2d.noise_sigma,
+            fit_degrees=FIT_DEGREES,
+            nx=NX,
+            x0x1_min=X0X1_MIN,
+            x0x1_max=X0X1_MAX,
+            noise_sigma=NOISE_SIGMA,
+            coeff_signal_to_noise=COEFF_SIGNAL_TO_NOISE,
         )
         t1 = time.time()
         print(f"Wall time: {(t1 - t0):.2f}s")
@@ -178,10 +168,10 @@ if __name__ == "__main__":
             pickle.dump(results, fout)
 
     print("Calculating in-sample, out-of-sample residuals and summary statistics")
-    ins_residuals = {_d: results["zdata"] - results["predictions"][_d] for _d in DEGREES}
-    oos_residuals = {_d: results["zdata_new"] - results["predictions"][_d] for _d in DEGREES}
-    ins_rssn = pd.DataFrame({_d: (ins_residuals[_d]**2).mean(axis=(-2, -1)) for _d in DEGREES})
-    oos_rssn = pd.DataFrame({_d: (oos_residuals[_d]**2).mean(axis=(-2, -1)) for _d in DEGREES})
+    ins_residuals = {_d: results["zdata"] - results[f"pred_{_d}"] for _d in FIT_DEGREES}
+    oos_residuals = {_d: results["zdata_new"] - results[f"pred_{_d}"] for _d in FIT_DEGREES}
+    ins_rssn = pd.DataFrame({_d: (ins_residuals[_d]**2).mean(axis=(-2, -1)) for _d in FIT_DEGREES})
+    oos_rssn = pd.DataFrame({_d: (oos_residuals[_d]**2).mean(axis=(-2, -1)) for _d in FIT_DEGREES})
     relative_oos_rss = (oos_rssn.T / oos_rssn["true"].T).T
 
     # Plot in-sample RSS for simulation runs
